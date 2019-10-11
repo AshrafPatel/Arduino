0196f85fb6d1e6092cb4adbd53eccc2f5b8a1af8
Ashraf Patel <4shr4f@live.co.uk>
2019-10-11 07:29:34 -0400

    Created Arduino projects

---
+++ tonebuzzer/buzzercode.ino
@@ -0,0 +1,23 @@
+void setup()
+{
+  
+}
+
+void loop()
+{
+  tone(13, 500);
+  delay(100);
+  noTone(13);
+  tone(13, 400);
+  delay(200);
+  noTone(13);
+  tone(13, 300);
+  delay(300);
+  noTone(13);
+  tone(13, 200);
+  delay(400);
+  noTone(13);
+  tone(13, 100);
+  delay(500);
+  noTone(13);
+}
